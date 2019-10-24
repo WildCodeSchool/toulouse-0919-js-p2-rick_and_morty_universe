4b73e4bebd68a7184973d54cf7fcd3e4178826bf
Lucie Fontela <luciefontela@gmail.com>
2019-10-24 11:16:38 +0200

    modification 2

--- src/components/AccueilPage.jsx
+++ src/components/AccueilPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './AccueilPage.css';
 import logo1 from '../imageaccueil/logo1.png';
 import titreimage from '../imageaccueil/titreimage.png';
-import portailimg from '../imageaccueil/portailimg.png';
+import portalimg from '../imageaccueil/portalimg.png';
 
 function AccueilPage() {
   return (
@@ -10,7 +10,7 @@ function AccueilPage() {
       <img src={logo1} alt="logo accueil" className="logo-img" />
       <h1 className="title-accueil">Universe</h1>
       <img src={titreimage} alt="title logo" className="title-img" />
-      <img src={portailimg} alt="portail logo" className="portail-img" />
+      <img src={portalimg} alt="portail logo" className="portal-img" />
     </div>
   );
 }
