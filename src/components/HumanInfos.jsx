c383745418d269118871eb590f1f4f2d2a4eaa0c
Navaloniaina Rakotovao <tsiresy+1@gmail.com>
2019-10-24 11:30:49 +0200

    6eme commit ac G

---
+++ src/components/HumanInfos.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function HumanInfos({ humanInfos }) {
+  return (
+    <div className="HumanInfos">
+      <img src={humanInfos.image} alt={humanInfos.character} />
+      <ul className="bloc">
+        <li> name :{humanInfos.name} </li>
+        <li> type : {humanInfos.type} </li>
+        <li> gender : {humanInfos.gender} </li>
+        <li> origin : {humanInfos.origin} </li>
+        <li> location : {humanInfos.location} </li>
+      </ul>
+    </div>
+  );
+}
+
+/** (Ref destructuration)
+function HumanInfos({ humanInfos }) {
+  const {name, type, gender, origin, location} = humanInfos;
+  return (
+    <div className="HumanInfos">
+      <img src={image} alt={character} />
+      <ul className="bloc">
+        <li> name :{name} </li>
+        <li> type : {type} </li>
+        <li> gender : {gender} </li>
+        <li> origin : {origin} </li>
+        <li> location : {location} </li>
+      </ul>
+    </div>
+  );
+}
+*/
+
+export default HumanInfos;
