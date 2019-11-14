914f6d9025383b39932ae515f165611735cb0f42
Ismael REPIR <repirismael6@gmail.com>
2019-11-14 10:11:36 +0100

    suppression des fichiers résiduels

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo1 from '../imageaccueil/logo1.png';
-import './sidebar.css';
+import './Sidebar.css';
 import { slide as Menu } from 'react-burger-menu';
 import { Link } from 'react-router-dom';
 
